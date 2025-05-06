4971001a50294708194cf9d345d66a3572e89ee3
OleksandrSavchuk <sashasavchuk1935@gmail.com>
2025-05-06 22:44:51 +0300

    fixed UserMapper

--- src/main/java/com/example/marketplace/mapper/UserMapper.java
+++ src/main/java/com/example/marketplace/mapper/UserMapper.java
@@ -2,8 +2,18 @@ package com.example.marketplace.mapper;
 
 import com.example.marketplace.dto.UserDto;
 import com.example.marketplace.entity.User;
+import com.example.marketplace.entity.enums.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper extends Mappable<User, UserDto> {
+
+    @Mapping(target = "role", expression = "java(mapRole(userDto.getRole()))")
+    User toEntity(UserDto userDto);
+
+    default Role mapRole(String roleStr) {
+        return Role.valueOf("ROLE_" + roleStr.toUpperCase());
+    }
+
 }
